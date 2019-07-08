aa8ab6ad6e413e9eec22143ae430464f4f3962f8
alexey.bogush <alexey.bogush@worldapp.com>
2019-07-08 09:54:30 +0300

    2 commit

--- src/test/java/MyFirst.java
+++ src/test/java/MyFirst.java
@@ -1,5 +1,7 @@
 public class MyFirst {
     public static void main(String[] args) {
         System.out.println("Privet");
+        System.out.println("Dobroe");
+        System.out.println("utro");
     }
 }
